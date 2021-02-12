b583fb2f06f0ebd9271f8b10180f8565ddfe45fd
M. Nafis Al-Mukhdi <74487079+mnafisalmukhdi1@users.noreply.github.com>
2021-02-12 10:50:31 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,146 +1,48 @@
-/* global monogatari */
-
-// Define the messages used in the game.
-monogatari.action ('message').messages ({
-	'Help': {
-		title: 'Help',
-		subtitle: 'Some useful Links',
-		body: `
-			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
-			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
-		`
-	}
-});
-
-// Define the notifications used in the game
-monogatari.action ('notification').notifications ({
-	'Welcome': {
-		title: 'Welcome',
-		body: 'This is the Monogatari VN Engine',
-		icon: ''
-	}
-});
-
-// Define the Particles JS Configurations used in the game
-monogatari.action ('particles').particles ({
-
-});
-
-// Define the canvas objects used in the game
-monogatari.action ('canvas').objects ({
-
-});
-
-// Credits of the people involved in the creation of this awesome game
-monogatari.configuration ('credits', {
-
-});
-
-
-// Define the images that will be available on your game's image gallery
-monogatari.assets ('gallery', {
-
-});
-
-// Define the music used in the game.
+// Assets for Music
 monogatari.assets ('music', {
-
+    'rakha': 'mars-rakha.mp3',
 });
 
-// Define the voice files used in the game.
-monogatari.assets ('voices', {
-
-});
-
-// Define the sounds used in the game.
-monogatari.assets ('sounds', {
-
-});
-
-// Define the videos used in the game.
-monogatari.assets ('videos', {
-
-});
-
-// Define the images used in the game.
-monogatari.assets ('images', {
-
-});
-
-// Define the backgrounds for each scene.
+// Assets for Scenes and Backgrounds
 monogatari.assets ('scenes', {
-
+	'bg': 'masjid.jpg',
 });
 
-
-// Define the Characters
-monogatari.characters ({
-	'y': {
-		name: 'Yui',
-		color: '#5bcaff'
-	}
+// Assets for Voices
+monogatari.assets ('voice', {
 });
 
+// Component: Main Screen
+monogatari.component ('main-screen').template (() => {
+    return `
+        <h1>LATAR BELAKANG PERBEDAAN QIRAAT</h1>
+        <main-menu></main-menu>
+    `;
+});
+
+// Configuration for Credits
+monogatari.configuration ('credits', {
+	"Terima Kasih Kepada": {
+		"Tuhan Yang Maha Esa": "Allah SWT.", 
+		"Rasul-Nya, terutama": "Muhammad SAW.",
+	},
+	"Ulumul Quran": {
+		"STAI RAKHA Amuntai": "Prodi PAI",
+		"Dosen Pengampu": "Syahrani, S.Pd.I., M.M.",
+		"Pembuat Presentasi": "M. Nafis Al-Mukhdi",
+	},
+	"Gambar": {
+		"Opened Qur'an800x600x300.jpg": "el7bara (Flickr.com)",
+		"Mihrab Masjid Raya Al-Aqsha Merauke.jpg": "Marwan Mohammad",
+	},
+});
+
+// Script
 monogatari.script ({
-	// The game starts here.
-	'Start': [
-		'show scene #f7f6f6 with fadeIn',
-		'show notification Welcome',
-		{
-			'Input': {
-				'Text': 'What is your name?',
-				'Validation': function (input) {
-					return input.trim ().length > 0;
-				},
-				'Save': function (input) {
-					this.storage ({
-						player: {
-							name: input
-						}
-					});
-					return true;
-				},
-				'Revert': function () {
-					this.storage ({
-						player: {
-							name: ''
-						}
-					});
-				},
-				'Warning': 'You must enter a name!'
-			}
-		},
-		'y Hi {{player.name}} Welcome to Monogatari!',
-		{
-			'Choice': {
-				'Dialog': 'y Have you already read some documentation?',
-				'Yes': {
-					'Text': 'Yes',
-					'Do': 'jump Yes'
-				},
-				'No': {
-					'Text': 'No',
-					'Do': 'jump No'
-				}
-			}
-		}
-	],
-
-	'Yes': [
-		'y Thats awesome!',
-		'y Then you are ready to go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
+	'_SplashScreen': [
+		'play music rakha',
+		'show scene rakha with fadeIn duration 3s',
+		'wait 8000',
 		'end'
 	],
-
-	'No': [
-
-		'y You can do it now.',
-
-		'show message Help',
-
-		'y Go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
-	]
-});
+	});
